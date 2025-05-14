551412750158a69ba0df4d4ace27362cd48e3f1d
berylatieno <berylatieno30@gmail.com>
2025-05-14 12:08:46 +0300

    add agg/widow funcs

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -13,11 +13,17 @@ ORDER BY total_bookings DESC;
 -- Rank Properties by Total Number of Bookings (Window Function)
 
 SELECT 
-  p.property_id,
-  p.name AS property_name,
-  COUNT(b.booking_id) AS total_bookings,
-  RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
-FROM "Property" p
-LEFT JOIN "Booking" b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.name
-ORDER BY booking_rank;
+  property_id,
+  property_name,
+  total_bookings,
+  ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number
+FROM (
+  SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings
+  FROM "Property" p
+  LEFT JOIN "Booking" b ON p.property_id = b.property_id
+  GROUP BY p.property_id, p.name
+) AS booking_counts;
+
